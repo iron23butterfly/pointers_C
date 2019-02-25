4b8e74c7cd4fc1f1aaf5a314d83d14bdcbae0d4b
sandhya23ram <47956751+sandhya23ram@users.noreply.github.com>
2019-02-24 20:43:28 -0800

    Create ptrArrayFunc.c

---
+++ ptrArrayFunc.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int sumOfArrayElements (int arraySize, int *firstAddr)
+{
+    int i;
+    int sum = 0;
+    
+    for (i=0;i<arraySize;i++)
+    {
+        sum = sum + *(firstAddr+i);
+    }
+    
+    return sum;
+}
+
+void    main ()
+{
+    int a[] = {1,2,3,4,5};
+    int size = sizeof(a)/sizeof(int);
+ //   printf("\nsize of array: %d", size);
+    int sum = sumOfArrayElements(size, a);
+    printf("\nsum %d", sum);
+}
+
+/*
+OUTPUT
+
+sum = 15
+*/
